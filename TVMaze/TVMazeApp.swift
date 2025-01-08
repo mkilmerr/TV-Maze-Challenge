294007b2a888b455d65a055a408579a80119edde
longarinas <longarinas@AdminPrides-MacBook-Pro.local>
2025-01-08 16:36:45 -0300

    first commit

---
+++ TVMaze/TVMazeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TVMazeApp.swift
+//  TVMaze
+//
+//  Created by longarinas on 08/01/25.
+//
+
+import SwiftUI
+
+@main
+struct TVMazeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
